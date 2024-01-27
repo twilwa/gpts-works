775afdb6e1a1338980439c3ba199b3a1ba120cb8
yikes <111034762+twilwa@users.noreply.github.com>
2024-01-27 14:39:24 -0800

    mod some endpoints rq

--- index/components/llm.py
+++ index/components/llm.py
@@ -1,5 +1,5 @@
 import os
-from llama_index.llms import AzureOpenAI
+from llama_index.llms import OpenAI  # Change this line
 from llama_index.embeddings import OpenAIEmbedding
 from llama_index import set_global_service_context, ServiceContext
 from components.log import log
@@ -8,30 +8,22 @@ llm_engine = None
 
 
 def init_llm():
-    api_key = os.getenv("AZURE_API_KEY")
-    api_base = os.getenv("AZURE_API_BASE")
-    api_type = "azure"
-    api_version = os.getenv("AZURE_API_VERSION")
-    llm_model = os.getenv("AZURE_LLM_MODEL")
-    embed_model = os.getenv("AZURE_EMBED_MODEL")
+    api_key = os.getenv("OPENAI_API_KEY")  
+    api_base = "https://dev-hub.agentartificial.com"  
+    llm_model = os.getenv("OPENAI_LLM_MODEL")  
+    embed_model = os.getenv("OPENAI_EMBED_MODEL")  
 
-    llm_engine = AzureOpenAI(
+    llm_engine = OpenAI(  
         model=llm_model,
-        engine=llm_model,
         api_key=api_key,
         api_base=api_base,
-        api_type=api_type,
-        api_version=api_version,
-        log=False,
     )
 
     embed_engine = OpenAIEmbedding(
         model=embed_model,
         deployment_name=embed_model,
         api_key=api_key,
-        api_base=api_base,
-        api_type=api_type,
-        api_version=api_version,
+        api_base="https://api.openai.com",  # Changed this line to point to regular OpenAI
     )
 
     service_context = ServiceContext.from_defaults(
